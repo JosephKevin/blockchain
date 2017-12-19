6efb1f069b62c4946b646025ebea47edfebc3662
JosephKevin <jkm2155@columbia.edu>
2017-12-18 19:12:49 -0500

    added node registry

--- blockchain.py
+++ blockchain.py
@@ -211,5 +211,17 @@ def full_chain():
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'Please supply a valid list of nodes', 400
+    for node in nodes:
+        blockchain.register_node(node)
+    response = {'message': 'New nodes have been successfully added',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
